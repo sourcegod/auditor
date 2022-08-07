908e2a06253f91febeecc7cc799bbce30af07985
Coolbrother <coolbrother@sourcegod.com>
2022-08-08 01:17:21 +0200

    Adding file: (src/audiconf.py) for loading instrument configuration file.

---
+++ src/audiconf.py
@@ -0,0 +1,75 @@
+#! /usr/bin/env python
+"""
+    File: audiconf.py
+    Configuration manager for Auditor
+    Date: Sun, 07/08/2022
+    Author: Coolbrother
+"""
+
+import xml.etree.ElementTree as ET
+
+class AudiConf(object):
+    """ Configuration manager """
+    def __init__(self):
+        self._instru_lst = []
+
+    #-------------------------------------------
+
+    def load(self, filename):
+        """
+        load XML file
+        from AudiConf object
+        """
+        
+        root_note = None
+        self.instru_lst = []
+        try:
+            root_node = ET.parse(filename).getroot()
+        except FileNotFoundError as err:
+            print("Error: configuration file not found", err)
+            return False
+        
+        print(f"root_node: {root_node}")
+
+        for elem in root_node.findall('instrumentList/instrument'):
+            instru_dic = {}
+            instru_dic["id"] = elem.findtext('id')
+            instru_dic["filename"] = elem.findtext('filename')
+            instru_dic["name"] = elem.findtext('name')
+            instru_dic["volume"] = elem.findtext('volume')
+            instru_dic["isMuted"] = elem.findtext('isMuted')
+            instru_dic["pan_L"] = elem.findtext('pan_L')
+            instru_dic["pan_R"] = elem.findtext('pan_R')
+            
+            self.instru_lst.append(instru_dic)
+            
+        """
+        for item in self.instru_lst:
+            # print(item)
+            print(f"id: {item['id']}, filename: {item['filename']}, name: {item['name']}")
+            print(f"volume: {item['volume']}, isMuted: {item['isMuted']}, pan_L: {item['pan_L']}, pan_R: {item['pan_R']}")
+        """    
+        
+        return True            
+   
+    #-------------------------------------------
+    
+    def get_instruments(self):
+        """
+        returns instrument list
+        from AudiConf object
+        """
+
+        return self.instru_lst
+
+    #-------------------------------------------
+
+#========================================
+
+
+if __name__ == "__main__":
+    filename = "/var/tmp/test/drumkit_1.xml"
+    app = AudiConf()
+    app.load(filename)
+    input("It's OK...")
+#-------------------------------------------
