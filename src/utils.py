270cc46bb4a06ceb4319072825c514e95412b637
Coolbrother <coolbrother@sourcegod.com>
2022-08-10 23:12:44 +0200

    Adding: (change_data function in utils.py module)

--- src/utils.py
+++ src/utils.py
@@ -58,4 +58,33 @@ def limit_value(val, min_val=0, max_val=127):
 
 #-------------------------------------------
 
+def change_data(data_num, data_lst, step=0, adding=0):
+    """
+    generic change data
+    """
+
+    changing =0
+    val =0
+    new_data = None
+    if not data_lst:
+        return (data_num, new_data)
+
+    max_val = len(data_lst) -1
+    if adding == 0:
+        if step == -1:
+            step = max_val
+    else: # adding value
+        val = data_num + step
+        changing =1
+    if changing:
+        step = limit_value(val, 0, max_val)
+    if data_num != step:
+        data_num = step
+        new_data = data_lst[data_num]
+    else: # no change for menu num
+        beep()
+    
+    return (data_num, new_data)
+
+#-------------------------------------------
 
